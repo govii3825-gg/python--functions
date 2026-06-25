fd6f2ab669e48aecd4a28df6582bd5a0ccf34561
Govind unni <govii3825@gmail.com>
2026-06-25 19:04:55 +0300

    Add files via upload

---
+++ dc1.py
@@ -0,0 +1,90 @@
+# dictionary
+
+
+
+# # print(type(mydata))
+# mydata={}
+# mydata["name"]="govind"
+# mydata["age"]=24
+# mydata["placee"]= "kochi"
+# mydata["phone"]=9400303825
+# mydata["name"]="govii"
+# print(mydata)
+
+
+# data={"name":"mohan","age":24,"place":"kochi","phone":9400303825,}
+# print
+
+# functions
+
+# def saymyname():
+#     print("thomas shelby")
+# saymyname()
+# saymyname()
+# saymyname()
+# saymyname()
+# saymyname()
+
+# def add2(a,b=0):
+#     print(a,"a")
+#     print(b,"b")
+# # add2(5,6) positional function
+# add2(a=5,b=6)keyword function
+# add2(a=5)default
+
+# rteturn statement
+
+# def add2(a,b):
+#     c=a+b
+#     return c 
+#     print(c)
+# print(add2(3,4))
+
+# def add2(q,p):
+#     r=(q+p)
+#     return 
+#     print(r) 
+# print(add2(4,5))
+
+# factorial
+
+
+# sum=0
+# i=5
+# for i in range(1,i+1):
+#     sum=sum+i
+#     print(i)
+# n=int(input(";enter a number"))
+# fact=5
+# for i in range(1,fact+n):
+#     fact=fact+i
+#     print(fact)
+
+
+# def saymyname():
+#     print("robert donie jr")
+# saymyname()
+# saymyname()
+
+ 
+# def add2(a,j):
+#    print(a+j)
+#    print(a,"a,")
+#    print(j ,"j")
+ 
+# add2(a=5,j=4)
+
+
+
+# def add2(a,b):
+#     c=a+b
+#     return c
+#     print(c)
+# print(add2(5,6))
+    
+    
+# def add2(a,b):
+#     c=a+b
+#     return "govind"
+#     print(c)
+# print(add2(5,7)
